65005215278e1a064d62fc43f4de3f2b09856735
Muhammad Anas Raza <63569834+anas-r-dev@users.noreply.github.com>
2022-10-04 16:04:28 -0400

    Update specmix.py

--- specmix.py
+++ specmix.py
@@ -1,8 +1,23 @@
 import random
 import torch
 
+def get_band(x, min_band_size, max_band_size, band_type, mask):
+    assert band_type.lower() in ['freq', 'time'], f"band_type must be in ['freq', 'time']"
+    if band_type.lower() == 'freq':
+        axis = 2
+    else:
+        axis = 1
+    band_size =  random.randint(min_band_size, max_band_size)
+    mask_start = random.randint(0, x.size()[axis] - band_size) 
+    mask_end = mask_start + band_size
+    
+    if band_type.lower() == 'freq':
+        mask[:, mask_start:mask_end] = 1
+    if band_type.lower() == 'time':
+        mask[mask_start:mask_end, :] = 1
+    return mask
 
-def specmix(x, y, alpha, prob, f_band_size, t_band_size):
+def specmix(x, y, prob, min_band_size, max_band_size, max_frequency_bands=3, max_time_bands=3):
     if prob < 0:
         raise ValueError('prob must be a positive value')
 
@@ -10,16 +25,17 @@ def specmix(x, y, alpha, prob, f_band_size, t_band_size):
     if k > 1 - prob:
         batch_size = x.size()[0]
         batch_idx = torch.randperm(batch_size)
-        mask_start = random.randint(0, x.size()[2] - f_band_size)        
-        mask_end = mask_start + f_band_size
-        x[:, mask_start:mask_end, :] = x[batch_idx, mask_start:mask_end,: ]
-        lam_freq = (f_band_size / x.size()[2])
-        mask_start = random.randint(0, x.size()[1] - t_band_size)
-        mask_end = mask_start + t_band_size
-        x[:, :, mask_start:mask_end] = x[batch_idx, :, mask_start:mask_end]
-        lam_time = ((mask_end - mask_start) / x.size()[1])
-        lam = 1 - (lam_freq + lam_time - (f_band_size * t_band_size) / (x.size()[1] * x.size()[2]))
-        y = y * lam + y[batch_idx] * (1 - lam)
+        print(batch_idx)
+        mask = torch.zeros(x.size()[1:3])
+        num_frequency_bands = random.randint(1, max_frequency_bands)
+        for i in range(1, num_frequency_bands):
+            mask = get_band(x, min_band_size, max_band_size, 'freq', mask)
+        num_time_bands = random.randint(1, max_time_bands)
+        for i in range(1, num_time_bands):
+            mask = get_band(x, min_band_size, max_band_size, 'time', mask)
+        lam = torch.sum(mask) / (x.size()[1] * x.size()[2])
+        x = x * (1 - mask) + x[batch_idx] * mask
+        y = y * (1 - lam) + y[batch_idx] * (lam)
         return x, y
     else:
         return x, y
