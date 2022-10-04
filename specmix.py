cf215463d225c696a233703c97c8d787eb3639ce
Muhammad Anas Raza <63569834+anas-r-dev@users.noreply.github.com>
2022-10-04 00:30:30 -0400

    Update specmix.py

--- specmix.py
+++ specmix.py
@@ -10,14 +10,14 @@ def specmix(x, y, alpha, prob, f_band_size, t_band_size):
     if k > 1 - prob:
         batch_size = x.size()[0]
         batch_idx = torch.randperm(batch_size)
-        mask_start = random.randint(0, x.size()[1] - f_band_size)        
+        mask_start = random.randint(0, x.size()[2] - f_band_size)        
         mask_end = mask_start + f_band_size
         x[:, mask_start:mask_end, :] = x[batch_idx, mask_start:mask_end,: ]
-        lam_freq = (f_band_size / x.size()[1])
-        mask_start = random.randint(0, x.size()[2] - t_band_size)
+        lam_freq = (f_band_size / x.size()[2])
+        mask_start = random.randint(0, x.size()[1] - t_band_size)
         mask_end = mask_start + t_band_size
         x[:, :, mask_start:mask_end] = x[batch_idx, :, mask_start:mask_end]
-        lam_time = ((mask_end - mask_start) / x.size()[2])
+        lam_time = ((mask_end - mask_start) / x.size()[1])
         lam = 1 - (lam_freq + lam_time - (f_band_size * t_band_size) / (x.size()[1] * x.size()[2]))
         y = y * lam + y[batch_idx] * (1 - lam)
         return x, y
